729db4fac4df8f6dafa66d9d9a4401f85dcb181c
Sorokin0 <s.aleksander.99.09@gmail.com>
2022-09-11 10:22:17 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿//This is a comment to my mode
+
+//rakendus küsib kasuytajal sisestada tema nime
+//rakendus tervitab kasutajad nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne tüüb andmed
+string userName;
+
+userName = Console.ReadLine();
+
+Console.WriteLine($"Hello, {userName}!");
